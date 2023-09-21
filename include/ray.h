653801a7f49cf37aad67ac8fd025391c379cb7b4
Fenix9000 <93600617+Fenix9000@users.noreply.github.com>
2023-09-21 16:27:27 +0200

    Added variables
    Added variables that were given from the slides, probably missing some stuff

--- include/ray.h
+++ include/ray.h
@@ -2,15 +2,18 @@
 
 #include "../include/glm/glm.hpp"
 #include "../include/colorDBL.h"
+#include "../include/polygon.h"
 
 class Ray {
 public:
     glm::vec3 startVertex;
+    glm::vec3 endVertex;
     glm::vec3 direction;
     Ray* previousRay;
     Ray* nextRay;
     ColorDBL color;
+    polygon* startingVertexSurface;
 
     // Constructor
-    Ray(glm::vec3 start, glm::vec3 direction,ColorDBL color = ColorDBL(0.0, 0.0, 0.0), Ray* prev = nullptr, Ray* next = nullptr);
+    Ray(glm::vec3 start, glm::vec3 direction, ColorDBL color = ColorDBL(0.0, 0.0, 0.0), Ray* prev = nullptr, Ray* next = nullptr);
 };
--- source/ray.cpp
+++ source/ray.cpp
@@ -1,7 +1,7 @@
 #include "../include/ray.h"
 
+
 Ray::Ray(glm::vec3 start, glm::vec3 direction, ColorDBL color, Ray* prev, Ray* next)
-    : startVertex(start), direction(direction), color(color), previousRay(prev), nextRay(next)  {
+: startVertex(start), direction(direction), previousRay(prev), nextRay(next), color(color)  {
     glm::normalize(direction); // Normalizes the direction vector
 }
-
